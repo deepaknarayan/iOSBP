b7de6aff4f1e6ba90d564edce7acac060c1fd45a
Deepak Narayan <deepak@emdesic.com>
2014-06-23 01:47:39 +0200

    Initial Commit

---
+++ iOSBP/EMAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  EMAppDelegate.h
+//  iOSBP
+//
+//  Created by Deepak Velekkat on 23/06/14.
+//  Copyright (c) 2014 Deepak Velekkat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ iOSBPTests/iOSBPTests.m
@@ -0,0 +1,34 @@
+//
+//  iOSBPTests.m
+//  iOSBPTests
+//
+//  Created by Deepak Velekkat on 23/06/14.
+//  Copyright (c) 2014 Deepak Velekkat. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSBPTests : XCTestCase
+
+@end
+
+@implementation iOSBPTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
